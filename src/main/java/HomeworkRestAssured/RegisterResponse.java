253b31cccef64ef93c8ba1e91369d07423795f85
Kandyb90 <arturkandybovic1990@gmail.com>
2025-05-13 11:39:56 +0300

    Update RegisterResponse.java

--- src/main/java/HomeworkRestAssured/RegisterResponse.java
+++ src/main/java/HomeworkRestAssured/RegisterResponse.java
@@ -5,9 +5,4 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RegisterResponse {
     public String id;
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-
-    public static class Data {
-           }
 }
