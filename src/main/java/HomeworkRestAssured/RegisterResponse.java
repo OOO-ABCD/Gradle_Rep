3a03f73c0fae6ca265cb837bd36808788808f890
Kandyb90 <arturkandybovic1990@gmail.com>
2025-05-13 11:35:40 +0300

    Update RegisterResponse.java

--- src/main/java/HomeworkRestAssured/RegisterResponse.java
+++ src/main/java/HomeworkRestAssured/RegisterResponse.java
@@ -1,25 +1,13 @@
 package HomeworkRestAssured;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RegisterResponse {
     public String id;
-    public String name;
-    public String createdAt;
-    public Data data;
 
     @JsonIgnoreProperties(ignoreUnknown = true)
 
     public static class Data {
-        public int year;
-        public double price;
-
-        @JsonProperty ("CPU model")
-        public String cpuModel;
-
-        @JsonProperty ("Hard disk size")
-        public String hardDiskSize;
-    }
+           }
 }
